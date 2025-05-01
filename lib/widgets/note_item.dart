b2a31c0cfd570d93babe0ad81c32702335065de2
m9stafa05 <m9stafa05@gmail.com>
2025-05-01 20:27:39 +0300

    Create Note Item

---
+++ lib/widgets/note_item.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+      margin: const EdgeInsets.only(top: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter Tips',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: Text(
+                'Build beautiful apps with Flutter. ',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey[600],
+                ),
+              ),
+            ),
+            trailing: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: IconButton(
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.black,
+                  size: 30,
+                ),
+                onPressed: () {
+                  // Handle delete action
+                },
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Text(
+              'Created on: 2023-10-01',
+              style: TextStyle(fontSize: 12, color: Colors.grey[600]),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
