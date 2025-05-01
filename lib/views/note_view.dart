339906691d14960930f6245f4556a3925f938e9d
m9stafa05 <m9stafa05@gmail.com>
2025-05-01 19:28:23 +0300

    Adding NoteViewBody

--- lib/views/note_view.dart
+++ lib/views/note_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widgets/note_view_body.dart';
 
 class NoteView extends StatelessWidget {
   const NoteView({super.key});
-static const String id = '/noteView';
+  static const String id = '/noteView';
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: const NoteViewBody(),
+    );
   }
 }
