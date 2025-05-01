0177144fb6bd707b079623ceba9b385a2862aec3
m9stafa05 <m9stafa05@gmail.com>
2025-05-01 18:50:21 +0300

    create note view and change the theme

---
+++ lib/views/note_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NoteView extends StatelessWidget {
+  const NoteView({super.key});
+static const String id = '/noteView';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
