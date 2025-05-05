0dfbe5ab519656937220b43ea6cf590dff677d08
m9stafa05 <m9stafa05@gmail.com>
2025-05-05 21:12:36 +0300

    Add functionality to FloatingActionButton to show AddNoteBottomSheet for note creation

--- lib/views/note_view.dart
+++ lib/views/note_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widgets/add_note_bottom_sheet.dart';
 import 'package:note_app/widgets/note_view_body.dart';
 
 class NoteView extends StatelessWidget {
@@ -8,7 +9,18 @@ class NoteView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            shape: RoundedRectangleBorder(
+              
+              borderRadius: BorderRadius.circular(16),
+            ),
+            context: context,
+            builder: (context) {
+              return const AddNoteBottomSheet();
+            },
+          );
+        },
         child: const Icon(Icons.add),
       ),
       body: const NoteViewBody(),
