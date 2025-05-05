7125654776dede3c664ccfd598e8d38c94e737ea
m9stafa05 <m9stafa05@gmail.com>
2025-05-05 22:38:17 +0300

    Add EditNoteView and update NoteItem to navigate to it

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,60 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              'Flutter Tips',
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, EditNoteView.id);
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                'Flutter Tips',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Text(
+                  'Build beautiful apps with Flutter. ',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey[600],
+                  ),
+                ),
+              ),
+              trailing: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: IconButton(
+                  icon: Icon(
+                    Icons.delete,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                  onPressed: () {
+                    // Handle delete action
+                  },
+                ),
               ),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
               child: Text(
-                'Build beautiful apps with Flutter. ',
+                'Created on: 2023-10-01',
                 style: TextStyle(
-                  fontSize: 16,
+                  fontSize: 12,
                   color: Colors.grey[600],
                 ),
               ),
             ),
-            trailing: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              child: IconButton(
-                icon: Icon(
-                  Icons.delete,
-                  color: Colors.black,
-                  size: 30,
-                ),
-                onPressed: () {
-                  // Handle delete action
-                },
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: Text(
-              'Created on: 2023-10-01',
-              style: TextStyle(fontSize: 12, color: Colors.grey[600]),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
