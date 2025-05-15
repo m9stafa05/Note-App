00706e841236edf923f9b360c17c7c0701d2d57c
m9stafa05 <m9stafa05@gmail.com>
2025-05-15 22:01:13 +0300

    update Main Color

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 const Color kModeColor = Color(0xFF393939);
-const Color kPrimaryColor = Color.fromARGB(255, 1, 175, 85);
+const Color kPrimaryColor = Color.fromARGB(255, 0, 158, 211);
 const String kNoteBox = 'note_box';
