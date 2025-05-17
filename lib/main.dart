816cfb9a025d60a33b53475a269c0a46b9f8c55c
m9stafa05 <m9stafa05@gmail.com>
2025-05-17 14:15:49 +0300

    Refactor main.dart to integrate MultiBlocProvider for AddNoteCubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/constants.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:note_app/models/note_model.dart';
 import 'package:note_app/views/edit_note_view.dart';
 import 'package:note_app/views/note_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 // flutter build web --release && firebase init hosting && firebase deploy --only hosting -m 'V 1.0.0'
 // firebase deploy --only hosting -m ' V 1.0.1'
@@ -19,17 +21,24 @@ class NoteApp extends StatelessWidget {
   const NoteApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        NoteView.id: (context) => const NoteView(),
-        EditNoteView.id: (context) => const EditNoteView(),
-      },
-      initialRoute: NoteView.id,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: const Color(0xFF393939),
-        fontFamily: 'Poppins',
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AddNoteCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        routes: {
+          NoteView.id: (context) => const NoteView(),
+          EditNoteView.id: (context) => const EditNoteView(),
+        },
+        initialRoute: NoteView.id,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          scaffoldBackgroundColor: const Color(0xFF393939),
+          fontFamily: 'Poppins',
+        ),
       ),
     );
   }
