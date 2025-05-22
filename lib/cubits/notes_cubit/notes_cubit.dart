2a7685817ef8c11d721b2a0f011d603a2775e667
m9stafa05 <m9stafa05@gmail.com>
2025-05-23 00:54:29 +0300

    Refactor NotesCubit and NotesState update NoteListView to use BlocBuilder

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,5 +1,7 @@
+// ignore: depend_on_referenced_packages
 import 'package:bloc/bloc.dart';
 import 'package:hive/hive.dart';
+// ignore: depend_on_referenced_packages
 import 'package:meta/meta.dart';
 import 'package:note_app/constants.dart';
 import 'package:note_app/models/note_model.dart';
@@ -7,13 +9,10 @@ import 'package:note_app/models/note_model.dart';
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
-  NotesCubit() : super(NotesCubitInitial());
+  NotesCubit() : super(NotesInitial());
+  List<NoteModel>? notes;
   fetchAllNotes() {
-    try {
-      var noteBox = Hive.box<NoteModel>(kNoteBox);
-      emit(NotesCubitSuccess(noteBox.values.toList()));
-    } catch (e) {
-      emit(NotesCubitFailure(e.toString()));
-    }
+    var noteBox = Hive.box<NoteModel>(kNoteBox);
+    notes = noteBox.values.toList();
   }
 }
--- lib/widgets/note_list_view.dart
+++ lib/widgets/note_list_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/notes_cubit/notes_cubit.dart';
+import 'package:note_app/models/note_model.dart';
 import 'package:note_app/widgets/note_item.dart';
 
 class NoteListView extends StatelessWidget {
@@ -6,12 +9,18 @@ class NoteListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: EdgeInsets.zero,
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 8),
-          child: NoteItem(),
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+        List<NoteModel> notes =BlocProvider.of<NotesCubit>(context).notes ?? [];
+        return ListView.builder(
+          itemCount: notes.length,
+          padding: EdgeInsets.zero,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: NoteItem(),
+            );
+          },
         );
       },
     );
