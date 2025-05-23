60b3d266092ef8f0ec1f75001401fdc20b6b0f6f
m9stafa05 <m9stafa05@gmail.com>
2025-05-23 03:41:01 +0300

    Refactor NoteViewBody to use StatefulWidget and fetch notes in initState

--- lib/widgets/note_view_body.dart
+++ lib/widgets/note_view_body.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:note_app/widgets/custom_app_bar.dart';
 import 'package:note_app/widgets/note_list_view.dart';
 
-class NoteViewBody extends StatelessWidget {
+class NoteViewBody extends StatefulWidget {
   const NoteViewBody({super.key});
+
+  @override
+  State<NoteViewBody> createState() => _NoteViewBodyState();
+}
+
+class _NoteViewBodyState extends State<NoteViewBody> {
+  @override
+
+  void initState() {
+    BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +26,7 @@ class NoteViewBody extends StatelessWidget {
       child: Column(
         children: [
           CustomAppBar(
-            title: 'Notes App',
+            title: 'Notes',
             icon: Icons.search,
             onPressed: () {},
           ),
