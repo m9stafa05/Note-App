8bf6e87d3ccb635949cb6c1e5df5688817546030
m9stafa05 <m9stafa05@gmail.com>
2025-05-23 19:25:58 +0300

    Add ColorListView  to AddNoteForm

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:note_app/models/note_model.dart';
+import 'package:note_app/widgets/colors_list_view.dart';
 import 'package:note_app/widgets/custom_bottom.dart';
 import 'package:note_app/widgets/custom_text_field.dart';
 
@@ -39,6 +40,8 @@ class _AddNoteFormState extends State<AddNoteForm> {
             },
           ),
           const SizedBox(height: 32),
+          const ColorListView(),
+          const SizedBox(height: 32),
           CustomBottomSheet(
             onTap: () {
               if (formKey.currentState!.validate()) {
@@ -64,6 +67,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
     );
   }
 }
+
 String formatFriendlyDate(DateTime date) {
   final now = DateTime.now();
   final today = DateTime(now.year, now.month, now.day);
