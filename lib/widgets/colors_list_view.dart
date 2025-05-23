d3dabdb29aef1c369d5b96d28a7ecba5b373b98f
m9stafa05 <m9stafa05@gmail.com>
2025-05-24 00:02:42 +0300

    Add color selection functionality to ColorListView

--- lib/widgets/colors_list_view.dart
+++ lib/widgets/colors_list_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
 
 class ColorItem extends StatelessWidget {
   const ColorItem({
@@ -55,6 +57,8 @@ class _ColorListViewState extends State<ColorListView> {
               onTap: () {
                 setState(() {
                   currentIndex = index;
+                  BlocProvider.of<AddNoteCubit>(context).color =
+                      colors[index];
                 });
               },
               child: ColorItem(
